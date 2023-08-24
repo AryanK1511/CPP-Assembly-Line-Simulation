3bfd665cac9c2c67a8673c32a5f41ec931c374cd
Aryan Khurana <101019909+AryanK1511@users.noreply.github.com>
2023-08-24 11:55:16 -0400

    Update Station.cpp

--- Station.cpp
+++ Station.cpp
@@ -1,11 +1,3 @@
-// Name: Aryan Khurana
-// Seneca Student ID: 145282216
-// Seneca email: akhurana22@myseneca.ca
-// Date of completion: 06 April 2023
-//
-// I confirm that I am the only author of this file
-//   and the content was created entirely by me.
-
 #include "Station.h"
 #include <iostream>
 #include <iomanip>
